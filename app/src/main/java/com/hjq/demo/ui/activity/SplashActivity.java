d9aa5056f1a8794b7efb07ba1bff45b95591e15c
Jeff juhui-pc <330132662@qq.com>
2022-07-29 13:59:18 +0800

    50 圆子到手

--- app/src/main/java/com/hjq/demo/ui/activity/SplashActivity.java
+++ app/src/main/java/com/hjq/demo/ui/activity/SplashActivity.java
@@ -57,7 +57,8 @@ public final class SplashActivity extends AppActivity {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            BrowserActivity.start(getContext(), "http://www.baidu.com");//TODO url 绝对不可提交到版本库
+            String url = "http://www.baidu.com";
+            BrowserActivity.start(getContext(), url);//TODO url 绝对不可提交到版本库
             finish();
         }
     };
